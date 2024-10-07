6900de84d7ea4beaf449dcb3b6cb052bade96359
jysong0101 <juyeop0101@khu.ac.kr>
2024-10-07 21:29:39 +0900

    last commit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-
+    posts = Post.objects.all()  # 모든 게시물을 가져옴
+    return render(request, 'blog/post_list.html', {'posts': posts})
