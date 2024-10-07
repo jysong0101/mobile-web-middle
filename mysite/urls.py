c096e9bf66d2e309af3b261924447a4d5885cc7b
jysong0101 <juyeop0101@khu.ac.kr>
2024-10-07 20:13:38 +0900

    try commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include  # include를 추가합니다.
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),  # '' 경로로 blog.urls를 포함시킵니다.
+    path('admin/', admin.site.urls),  # a)
+    path('', include('blog.urls')),
 ]
+
+# added
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
